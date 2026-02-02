1a99cc5475b54d76d1aaa2766c023a290b71f675
Eliam Solis de Los Santos <eliamsolis2201@gmail.com>
2026-02-01 22:07:31 -0400

    refactor: add src folder
    Eso debio estar desde el inicio xd

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import tasksRouter from './routes/tasks.js'
+import tasksRouter from './src/routes/tasks.js'
 
 const port = 3000
 const app = express()
